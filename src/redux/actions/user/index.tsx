c80bd335c2e7f075fb99a76ab5f2f988b6dd0847
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2024-01-22 15:23:38 -0300

    initial commit

---
+++ src/redux/actions/user/index.tsx
@@ -0,0 +1,34 @@
+import * as types from '../../types'
+
+export type UserType = {
+    name?: string
+    avatar_url?: string
+}
+
+export function getUserRequest(name: string): {
+    type: string
+    payload: string
+} {
+    return {
+        type: types.GET_USER_REQUEST,
+        payload: name
+    }
+}
+
+export function getUserSuccess(user: UserType): {
+    type: string
+    payload: UserType
+} {
+    return{
+        type: types.GET_USER_SUCCESS,
+        payload: user
+    }
+}
+
+export function getUserFailure(name: string): {
+    type: string
+} {
+    return{
+        type: types.GET_USER_FAILURE,
+    }
+}
---
+++ src/redux/index.ts
@@ -0,0 +1,15 @@
+// import { createStore, applyMiddleware, Store, AnyAction } from 'redux';
+// import createSagaMiddleware from 'redux-saga';
+
+// const sagaMiddleware = createSagaMiddleware();
+
+// const store: Store<unknown, AnyAction> = createStore(
+//     rootReducer,
+//     applyMiddleware(sagaMiddleware)
+// )
+
+// sagaMiddleware.run(rootSaga);
+
+// export default store;
+// export * from './actions';
+// export * from './types';
